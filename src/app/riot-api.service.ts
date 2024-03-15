c18ba450a7194508c844a1a8035103d1bf9ea910
adrianrakiec <rakiecadrian17@gmail.com>
2024-03-15 16:09:01 +0100

    fix: delete test data from api service

--- src/app/riot-api.service.ts
+++ src/app/riot-api.service.ts
@@ -8,15 +8,11 @@ import { Summoner } from './types/Summoner';
 export class RiotApiService {
 	private readonly API_KEY: string =
 		'RGAPI-7a4343e1-0fbf-4fc9-b02f-db2a3a71f93b';
-	username: string = 'FlamingBeauty';
-	region: string = 'eun1';
-	regionFull: string = 'europe';
-	
 
 	constructor(private http: HttpClient) {}
 
-	getProfileData(region: string, username: string) {		
-    const apiUrl = `https://${region}.api.riotgames.com/lol/summoner/v4/summoners/by-name/${username}?api_key=${this.API_KEY}`;
-    return this.http.get<Summoner>(apiUrl);
+	getProfileData(region: string, username: string) {
+		const apiUrl = `https://${region}.api.riotgames.com/lol/summoner/v4/summoners/by-name/${username}?api_key=${this.API_KEY}`;
+		return this.http.get<Summoner>(apiUrl);
 	}
 }
