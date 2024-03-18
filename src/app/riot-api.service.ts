7e355068e8ad84a288e4a8207880c84502efe7f7
adrianrakiec <rakiecadrian17@gmail.com>
2024-03-18 14:27:21 +0100

    feat(api): create api service

--- src/app/riot-api.service.ts
+++ src/app/riot-api.service.ts
@@ -1,18 +1,46 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Summoner } from './types/Summoner';
+import { League } from './types/League';
+import { MatchesList } from './types/MatchesList';
+import { Match } from './types/Match';
 
 @Injectable({
 	providedIn: 'root',
 })
 export class RiotApiService {
 	private readonly API_KEY: string =
-		'RGAPI-7a4343e1-0fbf-4fc9-b02f-db2a3a71f93b';
+		'RGAPI-9735012b-7637-4e5a-8a7e-a99693e7fbd7';
 
 	constructor(private http: HttpClient) {}
 
 	getProfileData(region: string, username: string) {
 		const apiUrl = `https://${region}.api.riotgames.com/lol/summoner/v4/summoners/by-name/${username}?api_key=${this.API_KEY}`;
 		return this.http.get<Summoner>(apiUrl);
+	}
+
+	getRankData(region: string, accountId: string) {
+		const apiUrl = `https://${region}.api.riotgames.com/lol/league/v4/entries/by-summoner/${accountId}?api_key=${this.API_KEY}`;
+		return this.http.get<League>(apiUrl);
+	}
+
+	getMatchesList(region: string, puuid: string) {
+		const fullNameRegion = this.changeRegion(region);
+
+		const apiUrl = `https://${fullNameRegion}.api.riotgames.com/lol/match/v5/matches/by-puuid/${puuid}/ids?start=0&count=5&api_key=${this.API_KEY}`;
+		return this.http.get<MatchesList[]>(apiUrl);
+	}
+
+	getMatchesInfoList(region: string, matches: MatchesList[]) {
+		const fullNameRegion = this.changeRegion(region);
+
+		return matches.map(matchId => {
+			const apiUrl = `https://${fullNameRegion}.api.riotgames.com/lol/match/v5/matches/${matchId}?api_key=${this.API_KEY}`;
+			return this.http.get<Match>(apiUrl);
+		});
+	}
+
+	changeRegion(region: string) {
+		return region === 'na1' ? 'americas' : 'europe';
 	}
 }
