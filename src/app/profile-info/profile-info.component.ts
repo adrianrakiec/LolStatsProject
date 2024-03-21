a06a1b60e2255b2ba1969b61e398e55bd7e3994d
adrianrakiec <rakiecadrian17@gmail.com>
2024-03-21 20:27:08 +0100

    insert main information into rank and profile components from api

--- src/app/profile-info/profile-info.component.ts
+++ src/app/profile-info/profile-info.component.ts
@@ -1,12 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Summoner } from '../types/Summoner';
 
 @Component({
-  selector: 'app-profile-info',
-  standalone: true,
-  imports: [],
-  templateUrl: './profile-info.component.html',
-  styleUrl: './profile-info.component.css'
+	selector: 'app-profile-info',
+	standalone: true,
+	imports: [CommonModule],
+	templateUrl: './profile-info.component.html',
+	styleUrl: './profile-info.component.css',
 })
 export class ProfileInfoComponent {
-
+  @Input() profileInfo: Summoner | null = null;
 }
--- src/app/profile-page/profile-page.component.ts
+++ src/app/profile-page/profile-page.component.ts
@@ -2,12 +2,16 @@ import { Component } from '@angular/core';
 import { SearchComponent } from '../search/search.component';
 import { ProfileInfoComponent } from '../profile-info/profile-info.component';
 import { RangComponent } from '../rang/rang.component';
+import { ProfileDataService } from '../services/profile-data.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
 	selector: 'app-profile-page',
 	standalone: true,
-	imports: [SearchComponent, ProfileInfoComponent, RangComponent],
+	imports: [SearchComponent, ProfileInfoComponent, RangComponent, CommonModule],
 	templateUrl: './profile-page.component.html',
 	styles: ``,
 })
-export class ProfilePageComponent {}
+export class ProfilePageComponent {
+	constructor(public profileInfoService: ProfileDataService) {}
+}
